9b42f25e3f21598109d05d965b5f1cb4e5136675
hxazy <3537994247@qq.com>
2024-09-19 22:04:26 +0800

    First commit

---
+++ MainActivity.java
@@ -0,0 +1,31 @@
+package com.kero.myapplication;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv_id;
+    String str = "4721AS";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        tv_id = findViewById(R.id.tv_id);
+
+        tv_id.setText("Hello, hx!\n");
+    }
+}
